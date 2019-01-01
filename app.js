3fbad22070df3fe05ce0721c4089194d1ab6cd9e
XXX <prashantkr314@gmail.com>
2019-01-01 22:02:58 +0530

    creatring router and request handler, for routing users request

--- app.js
+++ app.js
@@ -7,55 +7,24 @@
 // Dependencies
 const http = require('http');
 const url = require('url');
-const { StringDecoder } = require('string_decoder');
+// const { StringDecoder } = require('string_decoder');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 
 
 // The server should respond to all requests with a string
 const server = http.createServer((req, res) => {
-      console.log(req.url, " <<<<--- url string");
-
       // 1. get the url the user requested for and parse it
       let parsedUrl = url.parse(req.url, true); // --> true option is use to specify url module to call the querystring module to
       console.log(parsedUrl); //                       in order to get the parsed Url object,
 
 
-
-      /*  The querystring module provides utilities for parsing and formatting URL query strings.
-      It can be accessed using ---->   const querystring = require('querystring');
-
-      querystring.parse(str[, sep[, eq[, options]]])  <<<-----
-
-      str <string> The URL query string to parse
-      sep <string> The substring used to delimit key and value pairs in the query string. Default: '&'.
-      eq <string>. The substring used to delimit keys and values in the query string. Default: '='.
-      options <Object>
-
-      The querystring.parse() method parses a URL query string (str) into a collection of key and value pairs.
-
-      For example,  --->>> the query string   'foo=bar&abc=xyz&abc=123'  is parsed into:
-
-      {
-            foo: 'bar',
-            abc: ['xyz', '123']
-      }
-
-      The object returned by the querystring.parse() method does not prototypically inherit from the JavaScript Object.
-      This means that typical Object methods such as
-      obj.toString(), obj.hasOwnProperty(), and others are not defined and will not work.
-
-      */
-
       // 2. get the path
       let path = parsedUrl.pathname;
-      console.log(path, " <<<<<-- untrimmed path");
-
       let trimmedPath = path.replace(/^\/+|\/+$/g, '');
-      console.log(trimmedPath, " <<<-- untrimmed");
-
 
       // 2. get the query string as an object
-      let querystring = parsedUrl.query;
+      let querystringObject = parsedUrl.query;
 
       // 3. identify the request method --> NOTE: curl can do get request only, for other use postman
       let method = req.method.toUpperCase();
@@ -64,103 +33,99 @@ const server = http.createServer((req, res) => {
       let headers = req.headers;
 
 
-      /*
-      What is PayLoad ?
-
-      The term 'payload' is used to distinguish between the 'interesting' information in a
-      chunk of data or similar, and the overhead to support it.
-
-      It is borrowed from transportation, where it refers to the part of the load that 'pays':
-      for example, a tanker truck may carry 20 tons of oil, but the fully loaded vehicle weighs much more than that
-      - there's the vehicle itself, the driver, fuel, the tank, etc.
-      It costs money to move all these, but the customer only cares about (and pays for) the oil, hence, 'pay-load'.
-
-
-      In programming, the most common usage of the term is in the context of message protocols,
-      to differentiate the protocol overhead from the actual data.
-      Take, for example, a JSON web service response that might look like this (formatted for readability):
-
-      {
-            "status":"OK",
-            "data":
-            {
-                  "message":"Hello, world!"
-            }
-      }
-
-      In this example, the string Hello, world! is the payload, the part that the recipient is
-      interested in; the rest, while vital information, is protocol overhead.
-
-
-      Another notable use of the term is in malware.
-      Malicious software usually has two objectives: spreading itself, and performing some kind of modification on the target
-      system(delete files, compromise system security, call home, etc.).
-      The spreading part is the overhead, while the code that does the actual evil - doing is the payload.
-      */
-
-
       // 5. get the payload, if any --> we need string_decoder module for that
       let decoder = new StringDecoder('utf-8'); // need to tell what kind of char-set/encoding it can expect
       //                                       so it can decode it using that char-set, Generally for all JSON api it will be utf-8
       let buffer = ''; // empty string
 
-      // payload or Body that comes with an http request in the http server,  as a stream,
-      // So we need to collect that stream as it comes and when the stream is at the end  we need to combine it as whole
-      // so that we can figure out what the payload is. becasue as we recieve only bits of the payload we only get
-      // few characters at a time, But we are intrested in what the entire payload is once it finished streaming in.
-
-
-      // we can bind the function (of appending the stream into buffer) to an event that the request object emits
-      // that event is called data
-      let count = 0;
       req.on('data', function (data) {
             let decodedData = decoder.write(data);
-            console.log(count++, " --> ", decodedData);
             buffer += decodedData;
       });
 
       // reeq also emits another event 'end' which tell when its done streaming
       req.on('end', () => {
-            console.log("ENDING STREAM");
             buffer += decoder.end();
 
-            // 6.  send the response
-            res.end("Server Responded\n");
+            /* at this point we have got everything that the user might have sent us
+            we know the path that they requested for
+            we know the queryString that they might have sent us,
+            we know which http method by which they are sending the request
+            we know the hearders that thet are sending.
+            we know the payload that they are sending, if any
+            so we have all the data we need to process the user requeset
+            --> for now we just want to pacakge it in an object and send/route it to an request handler
 
-            // finally : log the request
-            console.log("\n\nurl requested  on : ", trimmedPath,
-            " with method ", method,
-            "\nand with these query String parameters:", querystring,
-            "\n request reciever with these headers :\n ", JSON.stringify(headers, null, 4),
-            "\n\n with payload : " , JSON.stringify(buffer, null, 4));
+            SO we need to define some request handlers, and set-up a routing structure ie a router
+            so that this http server can look at the request and route it to the handler it needs to go to
+            */
 
-            // Test :  curl localhost:3000/webpath?foo=bar&abc=xyz&abc=123
+            // construct the data object need to be sent to the user
+            let data = {
+                  'trimmedPath': trimmedPath,
+                  'querystringObject': querystringObject,
+                  'method': method,
+                  'headers': headers,
+                  'payload': buffer
+            };
+
+            // choose the handler this request should go to
+            let chosenHandler = typeof (router[trimmedPath]) !== 'undefined' ? router[trimmedPath] : handler.notFoundHandler;
+
+            // typeof is an operator that always returns a string, describing the type of a value. ---> IMPORTANT
+
+            // rout the request to the chosen handler
+            chosenHandler(data, function (statusCode = 200, payload ={}) {
+                  /* setting up default values in case the handler doesn't return one.
+                  it is similar to :
+                  statusCode = typeof (statusCode === 'number') ? statusCode : 200;
+                  payload = typeof (payload === 'object') ? payload : {};
+                  */
+                  // now we can't send an object to the user, so we convert this object into string and send it
+                  let payloadString = JSON.stringify(payload, 4, null);
+
+                  // send the response
+                  res.writeHead(statusCode);
+                  res.end(payloadString);
+
+                  // note we can perform task even after the res.end() or res.send()
+                  console.log("\nReturning this response : ", payloadString);
+            });
       });
-
-      // THe Big Question , So not every request has a payload/body eg. GET then woud the 'end' event would be called???
-      //  YES, the 'end'  will be called doesn't matter if there is a payload or not.
-      // if there is no payload the 'data' event will not be called
-      // in that case the buffer will be initialized with the empty string and nothing is going to get appended to it.
-      // and its still going to be ended and we will still be able to send the response
-
-
-      /*
-      Node.js deals heavily with streams
-
-      You can think of it basically as a pipe with a flow of water in it where the water is actually data
-      and the pipe is the stream. I suppose it's kind of a 2-way pipe if the stream is bi-directional.
-      It's basically a common abstraction that is placed upon things where there is a flow or
-      sequence of data in one or both directions.
-
-      common thing you might find is textual streams that allow you to write strings instead of bytes,
-      or some languages provide binary streams that allow you to write primitive types.
-
-      A common thing you'll find in textual streams is a character encoding,
-      */
-
 });
 
 
+// -------------------------------------------------------------------------------------
+
+// difining the request handlers
+const handler = {};
+
+// about handler
+handler.aboutHandler = function (data, callback) {
+      console.log("aboutHandler <<-- handled the request");
+      callback(202, { name: "about handler" });
+};
+/* so each of these handlers is going to be getting a big-block of data, which we collected above from the user.
+We are also goint to send it a callback, and we want the handler to call the call-back when done handling the request.
+and tell us 2 things.
+1. we want to callback a http status collected
+2. and a payload, and that should be an object. (we are choosing it to be object, because we are making this handler to work exclusively with json, ie JSON api's. But if we want we could return anything, number, string etc.)
+*/
+// not-found handler
+handler.notFoundHandler = function (data, callback) {
+      console.log("notFoundHandler <<-- handled the request");
+      callback(400);
+};
+
+// -------------------------------------------------------------------------------------
+
+// difining  a request router
+const router = {
+      'about': handler.aboutHandler
+};
+
+
+// -------------------------------------------------------------------------------------
 
 // Start the server, and have it listen on port 3000
 server.listen(3000, () => {
@@ -168,3 +133,32 @@ server.listen(3000, () => {
 });
 // this will keep the node.js event-loop busy by telling, it always have something new to do,
 // which is continue to listen on port 3000, so inorder to stop we would need to proactively kill the server
+
+
+// -------------------------------------------------------------------------------------
+
+
+
+
+
+// 🌈  ~ >> brew install node@10
+// Updating Homebrew...
+// ==> Downloading https://homebrew.bintray.com/bottles/node@10-10.15.0.mojave.bott
+// ######################################################################## 100.0%
+// ==> Pouring node@10-10.15.0.mojave.bottle.tar.gz
+// ==> Caveats
+// node@10 is keg-only, which means it was not symlinked into /usr/local,
+// because this is an alternate version of another formula.
+
+// If you need to have node@10 first in your PATH run:
+//   echo 'export PATH="/usr/local/opt/node@10/bin:$PATH"' >> ~/.bash_profile
+
+// For compilers to find node@10 you may need to set:
+//   export LDFLAGS="-L/usr/local/opt/node@10/lib"
+//   export CPPFLAGS="-I/usr/local/opt/node@10/include"
+
+// ==> Summary
+// 🍺  /usr/local/Cellar/node@10/10.15.0: 3,630 files, 45.6MB
+// 🌈  ~ >> brew update
+// Already up-to-date.
+// 🌈  ~ >> clear
